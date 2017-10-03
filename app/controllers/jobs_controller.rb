7153c1d3959fcf51e45f0cfa881ef64335a1deb3
imac <imac@imacdeiMac.local>
2017-10-03 11:00:24 +0800

    step 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -52,3 +52,14 @@ class JobsController < ApplicationController
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
+
+def index
+  @jobs = case params[:order]
+  when 'by_lower_bound'
+    Job.where(is_hidden: false).order('wage_lower_bound DESC')
+  when 'by_upper_bound'
+    Job.where(is_hidden: flase).order('wage_upper_bound DESC')
+  else
+    Job.where(is_hidden: false).order('created_at DESC')
+  end
+end
